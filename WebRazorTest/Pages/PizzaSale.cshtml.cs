5c1b87ade56816f56cfa3823fa896f704711bba1
Mehmet Akif İLYASOĞULLARI <github@mehmetakifilyasogullari.com.tr>
2023-03-13 20:53:42 +0300

    sayfalar oluşturuldu

---
+++ WebRazorTest/Pages/PizzaSale.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorTest.Pages
+{
+    public class PizzaSaleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
